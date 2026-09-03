73846e116d9114613e38ab1b14e64b595778801f
adm li <2293151837@qq.com>
2026-09-04 01:12:55 +0800

    Update get_live.py

--- script/get_live.py
+++ script/get_live.py
@@ -1,12 +1,14 @@
 import requests
+import re
+
+# 已经移除 fanmingming、iptv-org 两个失效源，你可以自行增删链接
 URL_LIST = [
-        "https://raw.githubusercontent.com/Supprise0901/TVBox_live/refs/heads/main/live.txt",
-        "https://raw.githubusercontent.com/bj123sd/hycg/refs/heads/main/tv.txt",
-        "https://raw.githubusercontent.com/lihansong888/collect-tv-txt/refs/heads/main/bbxx_lite.txt",
-        "https://raw.githubusercontent.com/zilong7728/Collect-IPTV/refs/heads/main/best_sorted.m3u"
-        
-        
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/refs/heads/main/live.txt",
+    "https://raw.githubusercontent.com/bj123sd/hycg/refs/heads/main/tv.txt",
+    "https://raw.githubusercontent.com/lihansong888/collect-tv-txt/refs/heads/main/bbxx_lite.txt",
+    "https://raw.githubusercontent.com/zilong7728/Collect-IPTV/refs/heads/main/best_sorted.m3u",
 ]
+
 CCTV_KEYWORDS = [
     "CCTV-1", "CCTV-2", "CCTV-3", "CCTV-4", "CCTV-5",
     "CCTV-5+", "CCTV-6", "CCTV-7", "CCTV-8", "CCTV-9",
@@ -52,17 +54,20 @@ def parse_any(text: str):
             fake_ext = f'#EXTINF:-1,{name_part}'
             res.append((fake_ext, url_part))
     return res
+
 def get_channel_name(extinf):
     if "," in extinf:
         return extinf.split(",")[-1].strip()
     return ""
+
 def add_group_tag(extinf: str, group_name:str):
-    if 'group-title=' in extinf:
-        return extinf
+    # 清除源自带全部旧分组标签，只使用我们定义的4个分组
+    extinf = re.sub(r' group-title="[^"]+"', '', extinf)
     idx = extinf.rfind(',')
     if idx == -1:
         return extinf
     return extinf[:idx] + f' group-title="{group_name}"' + extinf[idx:]
+
 def main():
     keep_list = []
     seen = set()
@@ -74,18 +79,15 @@ def main():
             for extinf, play_url in channels:
                 ch_name = get_channel_name(extinf)
                 group = None
-                # 判断央视
                 if any(k in ch_name for k in CCTV_KEYWORDS):
                     group = "央视频道"
-                # 判断卫视
                 elif any(k in ch_name for k in SATELLITE_KEYWORDS):
                     group = "卫视频道"
-                # 判断音乐频道
                 elif any(k in ch_name for k in MUSIC_KEYWORDS):
                     group = "音乐频道"
-                # 判断4K频道
                 elif any(k in ch_name for k in FOURK_KEYWORDS):
                     group = "4K频道"
+
                 if group is not None:
                     item_key = (extinf, play_url)
                     if item_key not in seen:
@@ -105,5 +107,6 @@ def main():
     with open("live.m3u", "w", encoding="utf-8") as f:
         f.write(final_text)
     print("✅已写入仓库根目录 live.m3u")
+
 if __name__ == "__main__":
     main()
