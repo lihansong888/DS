0c42608f81cdf2ac626354de455bd29c00f561b4
adm li <2293151837@qq.com>
2026-09-03 23:26:49 +0800

    Update get_live.py

--- script/get_live.py
+++ script/get_live.py
@@ -124,6 +124,8 @@ if __name__ == "__main__":
     REMOTE_URLS = [
         "https://live.445569.xyz/live.m3u",
         "https://raw.githubusercontent.com/Supprise0901/TVBox_live/refs/heads/main/live.txt",
+        "https://raw.githubusercontent.com/bj123sd/hycg/refs/heads/main/tv.txt",
+        "https://raw.githubusercontent.com/lihansong888/collect-tv-txt/refs/heads/main/bbxx_lite.txt",
         "https://raw.githubusercontent.com/zilong7728/Collect-IPTV/refs/heads/main/best_sorted.m3u"
     ]
     total_channels = []
